4f8ba66ea24f6b063da24887bc05e5c1ab59fa60
Sergei Prutsakov <psn206@mail.ru>
2024-09-05 16:34:48 +0300

    Добавьте файлы проекта.

---
+++ CreatingSheets/MethodRevitApp.cs
@@ -0,0 +1,77 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingSheets
+{
+    internal class MethodRevitApp
+    {
+
+        UIApplication uiApp;
+        UIDocument uiDoc;
+        Document doc;
+
+        public UIApplication UiApp { get => uiApp; }
+        public UIDocument UiDoc { get => uiDoc; }
+        public Document Doc { get => doc; }
+
+        public MethodRevitApp(ExternalCommandData commandData)
+        {
+            uiApp = commandData.Application;
+            uiDoc = UiApp.ActiveUIDocument;
+            doc = UiDoc.Document;
+        }
+
+        public List<FamilySymbol> GetListTitleBlock()
+        {
+            ElementCategoryFilter elementCategoryFilter = new ElementCategoryFilter(BuiltInCategory.OST_TitleBlocks);
+            ElementClassFilter elementClassFilter = new ElementClassFilter(typeof(FamilySymbol));
+            LogicalAndFilter logicalAndFilter = new LogicalAndFilter(elementCategoryFilter, elementClassFilter);
+            List<FamilySymbol> familySymbol = new FilteredElementCollector(Doc)
+              .WherePasses(logicalAndFilter)
+              .Cast<FamilySymbol>()
+              .ToList();
+            return familySymbol;
+        }
+
+        public List<View> GetListViews()
+        {
+            var views
+                = new FilteredElementCollector(doc)
+                    .OfClass(typeof(View))
+                    .Cast<View>()
+                    .Where(p => p.ViewType == ViewType.FloorPlan)
+                    .ToList();
+            return views;
+        }
+
+        public void СreateSheets(string numberSheets, string developer, FamilySymbol selectedVTitleBlock, View selectedView)
+        {
+
+            using (Transaction ts = new Transaction(Doc, "Create New Sheet"))
+            {
+                if (selectedView == null || selectedVTitleBlock == null) return;
+                int number = Convert.ToInt32(numberSheets);
+                if (number <= 0) number = 1;
+
+                ts.Start();
+                for (int i = 1; i <= number; i++)
+                {
+                    ElementId newViewID = selectedView.Duplicate(ViewDuplicateOption.AsDependent);
+                    ViewSheet sheet = ViewSheet.Create(Doc, selectedVTitleBlock.Id);
+                    Viewport viewport = Viewport.Create(Doc, sheet.Id, newViewID, new XYZ(0, 0, 0));
+                    sheet.Name = $"Лист{i} {selectedView.Name} ";
+                    Parameter disignet = sheet.get_Parameter(BuiltInParameter.SHEET_DESIGNED_BY);
+                    disignet.Set(developer);
+                }
+                doc.Regenerate();
+                ts.Commit();
+
+            }
+        }
+    }
+}
---
+++ CreatingSheets/Properties/MainViewViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingSheets.Properties
+{
+    internal class MainViewViewModel
+    {
+
+        MethodRevitApp methodRevitApp;
+        public DelegateCommand SaveCommand { get; }
+        public List<FamilySymbol> ListTitleBlock { get; set; } = new List<FamilySymbol>();
+        public FamilySymbol SelectedTitleBlock { get; set; }
+        public List<View> ListViews { get; set; } = new List<View>();
+        public View SelectedView { get; set; }
+        public string NumberSheets { get; set; }
+        public string Developer { get; set; }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            SaveCommand = new DelegateCommand(OnSaveCommand);
+            methodRevitApp = new MethodRevitApp(commandData);
+            ListTitleBlock = methodRevitApp.GetListTitleBlock();
+            ListViews = methodRevitApp.GetListViews();
+        }
+
+        private void OnSaveCommand()
+        {
+            methodRevitApp.СreateSheets(NumberSheets, Developer, SelectedTitleBlock, SelectedView);
+
+            RaiseCloseRecuest();
+        }
+
+        public event EventHandler CloseRecuest;
+
+        public void RaiseCloseRecuest()
+        {
+            CloseRecuest?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
